4250a3f60a26fb182b2db90b1140a59bfca0005e
Ivan Rodriguez Asqui <iragination@gmail.com>
2015-04-15 19:15:57 +1000

    Position items

--- public/js/sequence-editor.js
+++ public/js/sequence-editor.js
@@ -9,12 +9,22 @@ $(function(){
 	$('#add_btn').click(function(){
 		//log("sup");
 		return !$('#available option:selected').remove().appendTo('#current'); 
-	})
+	});
 	
 	$('#remove_btn').click(function(){
 		//log("sup");
 		return !$('#current option:selected').remove().appendTo('#available'); 
-	})
+	});
+	
+    $('.position_buttons button').click(function(){
+        var $op = $('#current option:selected'),
+            $this = $(this);
+        if($op.length){
+            ($this.data('dir') == 'up') ? 
+                $op.first().prev().before($op) : 
+                $op.last().next().after($op);
+        }
+    });
 	
 	function log(msg){
 		console && console.log && console.log(msg);
