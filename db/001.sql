a9454a75bdf4090fb5e8c63c9758bd20bb46e8fd
Ivan Rodriguez Asqui <iragination@gmail.com>
2015-04-17 12:44:28 +1000

    linux fix

--- db/001.sql
+++ db/001.sql
@@ -1,14 +1,14 @@
 CREATE TABLE `sequence` ( 
 `id` INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY ,  
 `name` VARCHAR(64) NOT NULL,
-`created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,  
+`created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,  
 `permissions_group` VARCHAR(250) NOT NULL 
 ) ENGINE = InnoDB;
 
 
-CREATE TABLE sequence_quiz` (
+CREATE TABLE `sequence_quiz` (
 `sequence_id` INT(11) NOT NULL ,  
 `quiz_id` INT(11) NOT NULL ,  
 `position` INT(11) NOT NULL DEFAULT '1' ,    
 PRIMARY KEY  (`sequence_id`, `quiz_id`) 
-) ENGINE = InnoDB
+) ENGINE = InnoDB;
