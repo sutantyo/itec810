a0b978349be74a9bfef0caded298974bc5e4e5ba
Ivan Rodriguez Asqui <iragination@gmail.com>
2015-05-01 23:04:48 +1000

    gray out pending rows from quiz listing

--- public/js/rqz.js
+++ public/js/rqz.js
@@ -3,3 +3,10 @@ function showExplanation(identifier) {
 	$("#question-hint-box").html( hint );
 	$("#question-hint-box").show();
 }
+
+
+// @author Ivan Rodriguez
+$(function(){
+	//gray out pending rows from quiz listing
+	$('td.pending').parent('tr').css('color', 'gray');
+})
