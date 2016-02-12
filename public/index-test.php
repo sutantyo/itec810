2c0805d12b401ca554a64e4368afe3526b76e47f
daniels <daniel.sutantyo@gmail.com>
2016-02-12 17:44:34 +1100

    added application.ini

---
+++ public/index-test.php
@@ -0,0 +1,44 @@
+<?php
+
+// Define path to application directory
+defined('APPLICATION_PATH')
+    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
+
+// Define application environment
+defined('APPLICATION_ENV')
+    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'development'));
+
+// Ensure library/ is on include_path
+set_include_path(implode(PATH_SEPARATOR, array(
+	APPLICATION_PATH,
+    realpath(APPLICATION_PATH . '/../library'),
+    get_include_path(),
+)));
+
+
+// Check to see if all required files exist
+if( !file_exists(APPLICATION_PATH . '/configs/application.ini') || !file_exists(APPLICATION_PATH . '/configs/general.php') ) {
+	die("Cannot Find application.ini / general.php");
+}
+if( !is_writable(APPLICATION_PATH . "/../tmp") ) {
+	die("Either ". realpath(APPLICATION_PATH . "/../") ."/tmp doesn't exist, or is not writable");
+}
+if( !extension_loaded('gd') ) {
+	die("The GD2 Extension for PHP is not enabled. Please enable it");
+}
+if( !extension_loaded('ldap') ) {
+	die("The LDAP Extension for PHP is not enabled. Please enable it");
+}
+
+require_once 'functions.php'; //for randset
+
+/** Zend_Application */
+require_once 'Zend/Application.php';
+
+// Create application, bootstrap, and run
+$application = new Zend_Application(
+    APPLICATION_ENV,
+    APPLICATION_PATH . '/configs/application.ini'
+);
+$application->bootstrap()
+            ->run();
